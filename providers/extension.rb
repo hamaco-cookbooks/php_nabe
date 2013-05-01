813285a397b5875f079e22086b2143220ae7b792
hamaco <hamanaka.kazuhiro@gmail.com>
2013-05-01 16:46:25 +0900

    use する前に拡張インストールすると落ちるのを修正

--- providers/extension.rb
+++ providers/extension.rb
@@ -5,10 +5,14 @@ action :install do
       environment "HOME" => "#{node["php-nabe"]["user_home_root"]}/#{new_resource.user}"
 
       code <<-EOH
-        current_version=`cat $HOME/app/php-nabe/current`
+        if [ -f "$HOME/app/php-nabe/current" ]; then
+          current_version=`cat $HOME/app/php-nabe/current`
+        fi
+
         $HOME/app/php-nabe/php-nabe use #{new_resource.version}
         $HOME/app/php-nabe/php-nabe ext-install #{new_resource.name}
-        $HOME/app/php-nabe/php-nabe use $current_version
+
+        test $current_version && $HOME/app/php-nabe/php-nabe use $current_version
       EOH
 
       not_if "#{node["php-nabe"]["user_home_root"]}/#{new_resource.user}/app/php-nabe/build/php-#{new_resource.version}/bin/php #{node["php-nabe"]["user_home_root"]}/#{new_resource.user}/app/php-nabe/lib/listext.php | grep #{new_resource.name}"
