6443d328aaa82f9d6d43413af40dd49d3ad424bc
hamaco <hamanaka.kazuhiro@gmail.com>
2013-12-22 17:16:09 +0900

    vagrantのUbuntuにmakeがなかったので追加

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,6 @@
 packages = case node[:platform_family]
 when "debian"
-  %w{curl automake1.9 libxml2-dev texinfo autoconf re2c git}
+  %w{curl automake1.9 libxml2-dev texinfo autoconf re2c make git}
 when "rhel"
   %w{libxml2-devel texinfo autoconf re2c git}
 else
